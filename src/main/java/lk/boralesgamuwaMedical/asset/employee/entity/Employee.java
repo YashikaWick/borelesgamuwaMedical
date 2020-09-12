05500e88fb74e5fe131c881a12290947de01a741
LalithK90 <asakahatapitiya@gmail.com>
2020-09-12 16:12:04 +0530

    AdditionalService was completed

--- src/main/java/lk/boralesgamuwaMedical/asset/employee/entity/Employee.java
+++ src/main/java/lk/boralesgamuwaMedical/asset/employee/entity/Employee.java
@@ -8,7 +8,6 @@ import lk.boralesgamuwaMedical.asset.commonAsset.model.Enum.Title;
 import lk.boralesgamuwaMedical.asset.commonAsset.model.FileInfo;
 import lk.boralesgamuwaMedical.asset.employee.entity.Enum.Designation;
 import lk.boralesgamuwaMedical.asset.employee.entity.Enum.EmployeeStatus;
-import lk.boralesgamuwaMedical.asset.message.entity.EmailMessage;
 import lk.boralesgamuwaMedical.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -86,10 +85,6 @@ public class Employee extends AuditEntity {
 
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
     private LocalDate dateOfAssignment;
-
-
-    @ManyToMany( mappedBy = "employees" )
-    private List<EmailMessage> emailMessages;
 
     @Transient
     private List< MultipartFile > files = new ArrayList<>();
